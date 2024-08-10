c9f64f8344aafbc48975d744859ec7298d954a68
MasterEnderman <xxmr.endermanxx@gmail.com>
2024-08-10 16:04:55 +0200

    jei compat refactor

--- src/main/java/v0id/aw/compat/jei/AWPlugin.java
+++ src/main/java/v0id/aw/compat/jei/AWPlugin.java
@@ -5,7 +5,7 @@ import mezz.jei.api.ingredients.IModIngredientRegistration;
 import mezz.jei.api.recipe.IRecipeCategoryRegistration;
 import net.minecraft.item.ItemStack;
 import v0id.aw.common.block.AWBlocks;
-import v0id.aw.common.recipe.AARecipes;
+import v0id.aw.common.recipe.AetheriumAnvilRecipes;
 import v0id.aw.common.recipe.MetalFormerRecipes;
 import v0id.aw.lib.AWConsts;
 
@@ -38,16 +38,16 @@ public class AWPlugin implements IModPlugin
     public void registerCategories(IRecipeCategoryRegistration registry)
     {
         registry.addRecipeCategories(
-                new Categories.MetalFormerCategory(registry.getJeiHelpers().getGuiHelper()),
-                new Categories.AnvilCategory(registry.getJeiHelpers().getGuiHelper()));
+                new MetalFormerCategory(registry.getJeiHelpers().getGuiHelper()),
+                new AnvilCategory(registry.getJeiHelpers().getGuiHelper()));
     }
 
     @Override
     public void register(IModRegistry iModRegistry)
     {
-        iModRegistry.addRecipes(MetalFormerRecipes.recipes.stream().map(Wrappers.MetalFormerWrapper::new).collect(Collectors.toList()), AWConsts.catIDMetalFormer);
+        iModRegistry.addRecipes(MetalFormerRecipes.recipes.stream().map(MetalFormerWrapper::new).collect(Collectors.toList()), AWConsts.catIDMetalFormer);
         iModRegistry.addRecipeCatalyst(new ItemStack(AWBlocks.FORGE_COMPONENT, 1, 4), AWConsts.catIDMetalFormer);
-        iModRegistry.addRecipes(AARecipes.recipes.stream().map(Wrappers.AnvilWrapper::new).collect(Collectors.toList()), AWConsts.catIDAnvil);
+        iModRegistry.addRecipes(AetheriumAnvilRecipes.recipes.stream().map(AnvilWrapper::new).collect(Collectors.toList()), AWConsts.catIDAnvil);
         iModRegistry.addRecipeCatalyst(new ItemStack(AWBlocks.FORGE_COMPONENT, 1, 5), AWConsts.catIDAnvil);
     }
 
